a2e7792fb74348314e21b683d3927df72bb82046
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2011-11-20 19:21:54 -0500

    - Agregamos un macro para instalar librerías.

--- config/install_conf.cmake
+++ config/install_conf.cmake
@@ -1,4 +1,22 @@
 
+# macro pensado para instalar librerías.
+# Los tipos de librerías son:
+#     - SHARED
+#     - STATIC
+macro(GDINSTALL_LIB TYPE)
+    foreach(VAL ${ARGN})
+        if(${CMAKE_HOST_SYSTEM} MATCHES "Linux")
+            install(TARGETS ${VAL} LIBRARY DESTINATION /usr/lib/)
+        elseif(${CMAKE_HOST_SYSTEM} MATCHES "Windows")
+            message("mejorar condición")
+            if(${TYPE} MATCHES "SHARED")
+                install(TARGETS ${VAL} LIBRARY DESTINATION bin COMPONENT libraries)
+            else()
+                install(TARGETS ${VAL} LIBRARY DESTINATION lib COMPONENT libraries)
+            endif()
+        endif()
+    endforeach(VAL)
+endmacro(GDINSTALL_LIB)
 
 # macro pensado para instalar cabeceras.
 macro(GDINSTALL_H DIR)
