385f6d189398a20e6573de3147f45ba81023254f
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2011-11-20 22:51:54 -0500

    Creamos la clase Window para el demo.

--- modules/demos/demos_mod.cmake
+++ modules/demos/demos_mod.cmake
@@ -1,2 +1,13 @@
 set(DEMO_DIR ${CMAKE_CURRENT_LIST_DIR})
 include_directories(${DEMO_DIR}/include)
+
+set(DEMO_MOC_HDRS
+${DEMO_DIR}/include/Window.hpp
+)
+
+qt4_wrap_cpp(DEMO_MOC_SRCS ${DEMO_MOC_HDRS})
+
+add_library( ldemo ${DEMO_MOC_SRCS})
+
+add_executable(demo ${DEMO_DIR}/src/demo.cpp)
+target_link_libraries(demo ldemo)
---
+++ modules/demos/include/Window.hpp
@@ -0,0 +1,17 @@
+
+#ifndef DEMO_WINDOW_HPP
+#define DEMO_WINDOW_HPP
+
+#include<QMainWindow>
+
+namespace Demo
+{
+
+class Window: public QMainWindow
+{
+Q_OBJECT
+};
+
+}
+
+#endif
